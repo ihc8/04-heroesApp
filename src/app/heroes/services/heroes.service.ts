5516540564fd5917fccbc97de7f2c2ab68e6ef30
ivan8hernandez <artugoa@gmail.com>
2024-01-18 17:16:44 +0100

    cambiosRutas

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError,of } from 'rxjs';
 import { Hero } from '../interface/hero.interface';
 import { environments } from 'src/app/environments/environment';
 
@@ -11,6 +11,10 @@ export class HeroesService {
   /*Aqui me estoy trayendo los heroes del json */
   getHeroes():Observable<Hero[]>{
   return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
+}
+getHeroeById(id:string):Observable<Hero|undefined>{
+  return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`).
+  pipe(catchError(error=>of(undefined)));
 }
 
 
